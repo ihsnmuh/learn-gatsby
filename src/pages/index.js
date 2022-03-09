e4857c320ebc267504670bb79390ee02427ac744
Muhammad Ihsan <ihsan@femaledaily.com>
2022-03-09 13:17:58 +0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/layout';
 
@@ -6,6 +6,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+        src='../images/hybrid-storytellers.jpg'
+      />
     </Layout>
   );
 };
